aabf4ebdc19ed4d4b385c05c264713ff24cec4a4
Natalle <natallemoura7@gmail.com>
2022-02-26 18:41:01 +0100

    Bare navbar component done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,31 @@
+import logo from '../assets/logo/logo-small.png'
+
 export default function Navbar() {
-  return(
-    "Navbar here"
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#about">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#projects">
+            Projects
+          </a>
+        </li>
+        <li>
+          <a href="#hero">
+            <img src={logo} alt="Initial's NM inside a circle" />
+          </a>
+        </li>
+        <li>
+          <a href="#tech">Tech</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
   );
 };
