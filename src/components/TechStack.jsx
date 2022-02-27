30629bf7c179c3237827bce9e9b8a7a790212e32
Natalle <natallemoura7@gmail.com>
2022-02-27 21:55:12 +0100

    Create Tech and Footer css

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,5 +1,17 @@
+import '../styles/parts/techstack.css';
+
 export default function TechStack() {
-  return(
-    "Tech stack here"
+  return (
+    <section className="techstack">
+      <div className="techstack-container">
+        <h1>Technologies I use</h1>
+        <p className="techstack-paragraph">
+          Here is the techology that I already know and also starting to learn.
+        </p>
+        <div className="tech-icons-container">
+          tech icons list here (maybe transform it into a component)
+        </div>
+      </div>
+    </section>
   );
 };
