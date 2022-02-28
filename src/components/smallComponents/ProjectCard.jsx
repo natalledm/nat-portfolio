5ad806e038ad959a4503f259a9ddfa6dd99166c3
Natalle Moura <natallemoura7@gmail.com>
2022-02-28 10:26:49 +0100

    create project card

--- src/components/smallComponents/ProjectCard.jsx
+++ src/components/smallComponents/ProjectCard.jsx
@@ -1,7 +1,19 @@
 export default function ProjectCard(projects) {
   // think about a way to show different photos or a thumbnail of the project
-  const {ProjectName} = projects
-  return(
+  const { ProjectName, isDone } = projects
+  // onClick em todo o div do card
+
+  if (!isDone) return (
+    // see if need background img in p or I can put a paragraph on top of the img
+    <div className="project-card">
+      <div className="card-content">
+        <img src="" alt="" className="not-done" />
+        <p className="project-name">{ProjectName}</p>
+      </div>
+    </div>
+  );
+
+  return (
     <div className="project-card">
       <div className="card-content">
         <img src="" alt="" />
--- src/components/smallComponents/modal.jsx
+++ src/components/smallComponents/modal.jsx
@@ -1,16 +1,15 @@
 export default function Modal(projects) {
-  const { projectImage, projectImageAlt, projectName,
-    projectDescription, projectTech, projectHosted, projectGit } = projects;
+  const { name, description, image, alt, techlist, hosted, git} = projects;
 
   return (
     <section className="modal">
       <div className="modal-content">
-        <img src={projectImage} alt={projectImageAlt} />
-        <h2 className="project-title">{projectName}</h2>
-        <p className="project-description">{projectDescription}</p>
-        <span className="project-tech">{projectTech}</span>
-        <a href={projectHosted} className="project-hosted"></a>
-        <a href={projectGit} className="project-hosted"></a>
+        <img src={image} alt={alt} />
+        <h2 className="project-title">{name}</h2>
+        <p className="project-description">{description}</p>
+        <span className="project-tech">{techlist}</span>
+        <a href={hosted} className="project-hosted"></a>
+        <a href={git} className="project-hosted"></a>
       </div>
     </section>
   );
