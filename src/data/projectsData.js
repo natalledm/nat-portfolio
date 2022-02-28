daf42676b8e701ecdf28523ead5a8d18761a1b81
Natalle <natallemoura7@gmail.com>
2022-02-28 20:33:27 +0100

    Project card linked with projects data

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -9,10 +9,26 @@ export const projects = [
       "Firebase",
       "Figma"
     ],
-    "image": "to be taken",
+    "image": "images/photos/lorenzo-herrera-unsplash.jpg",
     "alt": "to be written",
     "hosted": "https://eika-wish-list.web.app/",
     "git": "https://github.com/natalledm/wishlist-eika",
     "isFinished": true
+  },
+  {
+    "name": "Segundo projeto",
+    "description": "A todo list transformed into wishlist where you can add items and prices, remove items, and hide or show the purchased items",
+    "techlist": [
+      "Javascript",
+      "React",
+      "CSS",
+      "Firebase",
+      "Figma"
+    ],
+    "image": "images/photos/marek-piwnicki-unsplash.jpg",
+    "alt": "to be written",
+    "hosted": "https://eika-wish-list.web.app/",
+    "git": "https://github.com/natalledm/wishlist-eika",
+    "isFinished": false
   },
 ];
