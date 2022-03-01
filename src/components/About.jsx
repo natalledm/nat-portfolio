4ff8be441688e9e2edf6b031caa23faa73809048
Natalle <natallemoura7@gmail.com>
2022-03-01 23:08:29 +0100

    Organizing the css of the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,12 @@
-import illustration from '../assets/illustrations/pale-woman-works-with-computer-small.png';
+import womanComputer from '../assets/illustrations/pale-woman-works-with-computer-small.png';
+import womanSelfie from '../assets/illustrations/casual-life-3d-woman-taking-a-funny-photo-with-dog-1.png'
 import '../styles/components/about.css';
 
 export default function About() {
   return (
     <section className='about'>
       <div className="about-container">
+        <img src={womanComputer} alt="Pale woman works with computer" className='about-illustration' />
         <h1>About me</h1>
         <p className='about-paragraph'>
           I am a person that is passionate about technologies, colors, games, and everything related to words.
@@ -14,7 +16,9 @@ export default function About() {
           The love for words, games, and colors merged into the frontend development, something that I am loving to learn more and more.
           Currently I work with the triad HTML/CSS/JS and React. :)
         </p>
-        <img src={illustration} alt="Pale woman works with computer" className='about-illustration' />
+        <span className='image-container'>
+          <img src={womanSelfie} alt="Pale woman works with computer" className='about-illustration-second' />
+        </span>
       </div>
     </section>
   );
