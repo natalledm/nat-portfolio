6940da8d044ea2de1a7836b0379803f103303fe7
Natalle <natallemoura7@gmail.com>
2022-03-15 19:33:30 +0100

    Change About component and css

--- src/components/layout-main-screen/About.jsx
+++ src/components/layout-main-screen/About.jsx
@@ -1,13 +1,12 @@
 import womanComputerMobile from '../../assets/illustrations/pale-woman-works-with-computer-small.png';
 import womanComputerDesktop from '../../assets/illustrations/pale-woman-works-with-computer.png';
-import womanSelfie from '../../assets/illustrations/casual-life-3d-woman-taking-a-funny-photo-with-dog-1.png'
 import './about.css';
 
 export default function About() {
   return (
     <section className='about' id='about'>
       <div className="section-container">
-        <img src={womanComputerMobile} alt="Pale woman works with computer" className='about-img-mobile' />
+        <img src={womanComputerMobile} alt="Woman works with computer" className='about-img-mobile' />
         <div className='about-title-container'>
           <h1 className='about-title'>About me</h1>
         </div>
@@ -18,12 +17,11 @@ export default function About() {
               I have a background in Linguistics and I love reading, translating, and writing.
             </p>
             <p className='about-paragraph'>
-              The love for words, games, and colors merged into the frontend development, something that I am loving to learn more and more.
+              The love for words, games, and colors merged into the front-end development, something that I am loving to learn more and more.
               Currently I work with the triad HTML/CSS/JS and React. :)
             </p>
           </div>
-          <img src={womanComputerDesktop} alt="Pale woman works with computer" className='about-img-desktop' />
-          <img src={womanSelfie} alt="Pale woman works with computer" className='about-img-selfie' />
+          <img src={womanComputerDesktop} alt="Woman works with computer" className='about-img-desktop' />
         </div>
       </div>
     </section>
