dcec59227e52a3787aa56b53efab26d349cd6ae6
Natalle <natallemoura7@gmail.com>
2022-03-17 11:05:25 +0100

    Refactor nav

--- src/components/layout-main-screen/Navbar.jsx
+++ src/components/layout-main-screen/Navbar.jsx
@@ -1,35 +1,17 @@
-import logo from '../../assets/logo/NM.png';
-import './navbar.css';
+import logo from "../../assets/logo/NM.png";
+import "./navbar.css";
 
 export default function Navbar() {
   return (
-    <nav className='navbar'>
-      <ul className='navbar-list'>
-        <li className='navbar-list-item about-item'>
-          <a href="#about">
-            About
-          </a>
-        </li>
-        <li className='navbar-list-item projects-item'>
-          <a href="#projects">
-            Projects
-          </a>
-        </li>
-        <li className='logo-item'>
-          <a href="#hero">
-            <img src={logo} alt="Initial's NM inside a circle" className='logo' />
-          </a>
-        </li>
-        <li className='name-spelled-container'>
-          <span className='name-spelled'>Natalle Moura</span>
-        </li>
-        <li className='navbar-list-item tech-item'>
-          <a href="#tech">Tech</a>
-        </li>
-        <li className='navbar-list-item contact-item'>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
+    <nav className="navbar-container">
+      <div className="navbar-items">
+        <a href="#about" className="navbar-item about-item">About</a>
+        <a href="#projects" className="navbar-item projects-item">Projects</a>
+        <a href="#hero"><img src={logo} alt="Letters NM inside a circle" className="logo" /></a>
+        <span className="name-spelled">Natalle Moura</span>
+        <a href="#tech" className="navbar-item tech-item">Tech</a>
+        <a href="#contact" className="navbar-item contact-item">Contact</a>
+      </div>
     </nav>
   );
 };
