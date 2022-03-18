fd045f0c7dfb2a9881b906a0514967b35e8655a1
Natalle <natallemoura7@gmail.com>
2022-03-18 12:07:41 +0100

    Contact css done

--- src/components/other-layout-components/Footer.jsx
+++ src/components/other-layout-components/Footer.jsx
@@ -11,11 +11,11 @@ export default function Footer() {
   const socialNetwork = networkData.map((network, index) => <NetworkInfo key={index} icon={network.icon} url={network.url} />);
 
   return (
-    <footer className="footer-container section-container" id="contact">
+    <footer className="footer-container" id="contact">
       <div className="social-list">
         {socialNetwork}
       </div>
-      <p className="copyright">© 2022 • Natalle Moura</p>
+      <p className="copyright">© Natalle Moura • 2022</p>
     </footer>
   );
 };
